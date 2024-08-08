21f48a587841c0a28af055841c6b49f495db753f
Hazel Wu <tinazx056@gmail.com>
2024-08-08 17:27:27 +0800

    revise: HomeView

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,11 +1,17 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
+import Trc20View from '../views/Trc20View.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: '/',
+      name: 'trc',
+      component: Trc20View
+    },
+    {
+      path: '/home',
       name: 'home',
       component: HomeView
     },
@@ -15,7 +21,7 @@ const router = createRouter({
       // route level code-splitting
       // this generates a separate chunk (About.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue')
+      component: () => import('../views/StockView.vue')
     }
   ]
 })
---
+++ src/views/Trc20View.vue
@@ -0,0 +1,50 @@
+<script setup lang="ts">
+// Component
+import AppNavbar from '@/components/AppNavbar.vue'
+// Dialog
+import Alert from '@/components/Dialog/ShowAlert.vue'
+import DepositDialog from '@/components/Dialog/DepositDialog.vue'
+import WithdrawalDialog from '@/components/Dialog/WithdrawalDialog.vue'
+import ProfileDialog from '@/components/Dialog/ProfileDialog.vue'
+// Pinia
+import { storeToRefs } from 'pinia'
+import { useDialogStore } from '@/stores/dialog'
+
+const dialogStore = useDialogStore()
+const { showDepositDialog, showWithdrawalDialog } =
+  storeToRefs(dialogStore)
+</script>
+
+<template>
+  <main>
+    <Alert
+      class="alert"
+      :dialog="dialogStore.alertStatus"
+      :icon="dialogStore.icon"
+      :text="dialogStore.text"
+    />
+    <AppNavbar />
+
+    <!-- 存款、取款、個人資料燈箱 Start -->
+    <v-dialog v-model="showDepositDialog" max-width="600">
+      <DepositDialog />
+    </v-dialog>
+
+    <v-dialog
+      v-model="showWithdrawalDialog"
+      max-width="600"
+    >
+      <WithdrawalDialog />
+    </v-dialog>
+
+    <v-dialog
+      v-model="dialogStore.showProfileDialog"
+      max-width="600"
+    >
+      <ProfileDialog />
+    </v-dialog>
+
+    <iframe src="https://hazelwu.com"></iframe>
+    <!-- 存款、取款、個人資料燈箱 End -->
+  </main>
+</template>
