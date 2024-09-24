db19494ca35891e3e8ca58c1c3221ef3db56a613
Hazel Wu <tinazx056@gmail.com>
2024-09-24 15:06:57 +0800

    feat: add Auth Page

--- src/router/index.ts
+++ src/router/index.ts
@@ -2,6 +2,8 @@ import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 import Trc20View from '../views/Trc20View.vue'
 import UserActionViewInGame from '../views/UserActionViewInGame.vue'
+import Auth from '../views/Auth.vue'
+
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -20,6 +22,11 @@ const router = createRouter({
       path: '/home',
       name: 'home',
       component: HomeView
+    },
+    {
+      path: '/auth',
+      name: 'auth',
+      component: Auth
     },
     {
       path: '/stock',
---
+++ src/views/Auth.vue
@@ -0,0 +1,17 @@
+<script lang="ts">
+import { onMounted } from 'vue'
+import { useUserStore } from '@/stores/user'
+
+// Pinia Vuex
+const userStore = useUserStore()
+const { handleRegister } = userStore
+
+onMounted(async () => {
+  const urlParams = new URLSearchParams(
+    window.location.search
+  )
+  console.log('urlParams', urlParams)
+
+  // await handleRegister
+})
+</script>
