aba61ece8e9ae5de2647285b36f4d4ef86326579
Hazel Wu <tinazx056@gmail.com>
2024-09-24 15:24:08 +0800

    test: Auth

--- src/views/Auth.vue
+++ src/views/Auth.vue
@@ -14,8 +14,25 @@ onMounted(async () => {
   const urlParams = new URLSearchParams(
     window.location.search
   )
-  console.log('urlParams', urlParams)
 
-  // await handleRegister
+  // 擷取 query 參數
+  const queryKeys = [
+    'id',
+    'first_name',
+    'username',
+    'photo_url',
+    'auth_date'
+  ]
+  const userData = queryKeys.reduce(
+    (acc, key) => {
+      acc[key] = urlParams.get(key) || ''
+      return acc
+    },
+    {} as Record<string, string>
+  )
+
+  console.log('userData', userData)
+
+  // await handleRegister(userData)
 })
 </script>
